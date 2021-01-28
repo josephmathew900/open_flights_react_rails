6b6660abc8b118ac0a21c7c2fc2e76fc37428f19
josephmathew900 <josephmathew900@gmail.com>
2021-01-28 08:54:33 +0530

    basic form setup done

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import Header from "./Header";
+import styled from "styled-components";
+import ReviewForm from "./ReviewForm";
 
-const Airline = () => {
-  return <div>This is the Airlines#show view for our app.</div>;
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`;
+const Main = styled.div`
+  padding-left: 50px;
+`;
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setAirline(response.data);
+        setLoaded(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      {loaded && (
+        <Fragment>
+          <Column>
+            <Main>
+              <Header
+                attributes={airline.data.attributes}
+                reviews={airline.included}
+              />
+
+              <div className="reviews"></div>
+            </Main>
+          </Column>
+          <Column>
+            <ReviewForm
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              attributes={airline.data.attributes}
+              review={review}
+            />
+          </Column>
+        </Fragment>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Airline;
---
+++ app/javascript/components/Airline/ReviewForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ReviewForm = (props) => {
+  return (
+    <div className="Wrapper">
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          Have an experience with {props.attributes.name}? Share your review.
+        </div>
+        <div className="field">
+          <input
+            onChange={props.handleChange}
+            value={props.review.title}
+            type="text"
+            name="title"
+            placeholder="Review Title"
+          />
+        </div>
+        <div className="field">
+          <input
+            onChange={props.handleChange}
+            value={props.review.description}
+            type="text"
+            name="description"
+            placeholder="Review Description"
+          />
+        </div>
+        <div className="rating-container">
+          <div className="rating-title-text">Rate this Airline</div>
+          [Star Rating Goes Here]
+        </div>
+        <button type="submit">Submit Your Review</button>
+      </form>
+    </div>
+  );
+};
+
+export default ReviewForm;
