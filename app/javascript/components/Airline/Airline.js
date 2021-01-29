dad64942f6e5ea6bf5b8362c681dc53008575951
josephmathew900 <josephmathew900@gmail.com>
2021-01-29 20:15:51 +0530

    rating component done

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Header from "./Header";
 import styled from "styled-components";
 import ReviewForm from "./ReviewForm";
+import Review from "./Review";
 
 const Wrapper = styled.div`
   margin-left: auto;
@@ -55,7 +56,7 @@ const Airline = (props) => {
     axios
       .post("/api/v1/reviews", { review, airline_id })
       .then((response) => {
-        const included = [...airline.included, response.data];
+        const included = [...airline.included, response.data.data];
         setAirline({ ...airline, included });
         setReview({ title: "", description: "", score: 0 });
       })
@@ -70,6 +71,13 @@ const Airline = (props) => {
     setReview({ ...review, score });
   };
 
+  let reviews;
+  if (loaded && airline.included) {
+    reviews = airline.included.map((item, index) => {
+      return <Review key={index} attributes={item.attributes} />;
+    });
+  }
+
   return (
     <Wrapper>
       {loaded && (
@@ -80,8 +88,7 @@ const Airline = (props) => {
                 attributes={airline.data.attributes}
                 reviews={airline.included}
               />
-
-              <div className="reviews"></div>
+              {reviews}
             </Main>
           </Column>
           <Column>
